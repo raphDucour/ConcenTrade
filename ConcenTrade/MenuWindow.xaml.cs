35458546a0d92d1330add71f353f1ce90550451f
antoine <antoine.dupuy01@gmail.com>
2025-05-20 13:37:52 +0200

    MENUWINDOWS IMPLEMNETR SANS CLIQUE BOUTON

---
+++ ConcenTrade/MenuWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Concentrade
+{
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Page des paramètres à venir !");
+        }
+
+        private void Stats_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Page des statistiques à venir !");
+        }
+    }
+}
--- ConcenTrade/Questionnaire.xaml.cs
+++ ConcenTrade/Questionnaire.xaml.cs
@@ -2,40 +2,21 @@
 
 namespace Concentrade
 {
-    public partial class MainWindow : Window
+    public partial class Questionnaire : Window
     {
-        public MainWindow()
+        public Questionnaire()
         {
             InitializeComponent();
 
             // (optionnel) Pour vérifier la résolution détectée
             var screenWidth = SystemParameters.PrimaryScreenWidth;
             var screenHeight = SystemParameters.PrimaryScreenHeight;
-
-            // Si tu veux utiliser ces valeurs plus tard, tu peux les stocker
             // MessageBox.Show($"Résolution détectée : {screenWidth} x {screenHeight}");
         }
-
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-            string name = NameInput.Text;
-            if (!string.IsNullOrWhiteSpace(name))
-            {
-                Properties.Settings.Default.UserName = name;//enregistrement du nom d'utilisateur
-                Properties.Settings.Default.Save();//sauvegarde du nom d'utilisateur
-                GreetingText.Text = $"Bonjour {name} !";
-            }
-            else
-            {
-                GreetingText.Text = "Bonjour !";
-            }
-        }
-
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             string savedName = Properties.Settings.Default.UserName;
-
             if (!string.IsNullOrWhiteSpace(savedName))
             {
                 NameInput.Text = savedName;
@@ -43,10 +24,31 @@ namespace Concentrade
             }
         }
 
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                // Sauvegarde
+                Properties.Settings.Default.UserName = name;
+                Properties.Settings.Default.Save();
+
+                GreetingText.Text = $"Bonjour {name} !";
+
+                // Ouvre MenuWindow (à condition qu’il existe)
+                MenuWindow menu = new MenuWindow();
+                menu.Show();
+                this.Close();
+            }
+            else
+            {
+                GreetingText.Text = "Bonjour !";
+            }
+        }
 
         private void NameInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-
+            GreetingText.Text = "";
         }
     }
 }
